f08f55b3f12cce124e658a0b5abd0eda319a934f
Shivam Sharma <64611917+CtrlAltShivaam@users.noreply.github.com>
2023-07-19 01:56:25 +0530

    UI updated
    Search way changed

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,43 @@
+// Open and Close Navbar Menu
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const bgOverlay = document.querySelector(".overlay");
+
+if (burgerMenu && bgOverlay) {
+   burgerMenu.addEventListener("click", () => {
+      navbarMenu.classList.add("is-active");
+      bgOverlay.classList.toggle("is-active");
+   });
+
+   bgOverlay.addEventListener("click", () => {
+      navbarMenu.classList.remove("is-active");
+      bgOverlay.classList.toggle("is-active");
+   });
+}
+
+// Close Navbar Menu on Links Click
+document.querySelectorAll(".menu-link").forEach((link) => {
+   link.addEventListener("click", () => {
+      navbarMenu.classList.remove("is-active");
+      bgOverlay.classList.remove("is-active");
+   });
+});
+
+// Open and Close Search Bar Toggle
+const searchBlock = document.querySelector(".search-block");
+const searchToggle = document.querySelector(".search-toggle");
+const searchCancel = document.querySelector(".search-cancel");
+
+if (searchToggle && searchCancel) {
+   searchToggle.addEventListener("click", () => {
+      searchBlock.classList.add("is-active");
+   });
+
+   searchCancel.addEventListener("click", () => {
+      searchBlock.classList.remove("is-active");
+   });
+}
+
 
 // const API_KEY= "1d3a0eefa97b499d8fbc4ee93eeb40b7"; // Anuj bhaiya's api
 // const API_KEY= "276580cd84b948849f480e6b63bb2718";// 2043shivam api
@@ -62,13 +102,17 @@ function onNavItemClick(id) {
   curSelectedNav.classList.add("active");
 }
 
-const searchButton = document.getElementById("search-button");
 const searchText = document.getElementById("search-text");
 
-searchButton.addEventListener("click", () => {
+searchText.addEventListener("keydown", (event) => {
+  if(event.key === 'Enter') {
   const query = searchText.value;
   if (!query) return;
   fetchNews(query);
   curSelectedNav?.classList.remove("active");
   curSelectedNav = null;
+  event.preventDefault();
+  searchBlock.classList.remove("is-active");
+  }
 });
+
